910f27fe09e06258e9f922d7efd85faea98849f9
Yadin Yafe <yadin258@yahoo.com>
2024-05-22 23:47:06 +0300

    add ability to search by artist and title

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -10,7 +10,6 @@ type VideoCardProps = {
 }
 
 export const VideoCard = ({ video }: VideoCardProps) => {
-    const imgURL = "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg";
     return (
         <div className="bg-gray-100 p-4">
             <div className="flex flex-col">
@@ -21,7 +20,7 @@ export const VideoCard = ({ video }: VideoCardProps) => {
                     className='rounded-lg px-2 py-2'
                     alt="Picture of the artist"
                 />
-                <VideoInfo title={video.title} artist={video.title} year={video.release_year} />
+                <VideoInfo title={video.title} artist={video.artist} year={video.release_year} />
             </div>
         </div>
 
--- src/components/VideosMain.tsx
+++ src/components/VideosMain.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { VideoCard } from './VideoCard';
 import { Videos } from '@/api/types/videos';
 
@@ -8,10 +8,33 @@ type VideosMainProps = {
 }
 
 export const VideosMain = ({ videos }: VideosMainProps) => {
+    const [query, setQuery] = useState('');
+    const [filteredVideos, setFilteredVideos] = useState(videos?.videos || []);
+
+    useEffect(() => {
+        if (videos) {
+            const lowercasedQuery = query.toLowerCase();
+            const filtered = videos.videos.filter(video =>
+                video.artist.toString().toLowerCase().includes(lowercasedQuery) ||
+                video.title.toString().toLowerCase().includes(lowercasedQuery)
+            );
+            setFilteredVideos(filtered);
+        }
+    }, [query, videos]);
+
     return (
         <section>
-            <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                {videos && videos.videos.map((video, idx) => (
+            <div className="mb-4 p-4">
+                <input
+                    type="text"
+                    placeholder="Search by artist or title"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="w-1/3 p-2 border rounded text-black font-semibold focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                />
+            </div>
+            <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4">
+                {filteredVideos.map((video, idx) => (
                     <VideoCard video={video} key={idx} />
                 ))}
             </div>
