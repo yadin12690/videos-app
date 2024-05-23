54a2cf1a87085e875e166cb25c012f00c2f14d98
Yadin Yafe <yadin258@yahoo.com>
2024-05-23 20:40:16 +0300

    remove paddding mobile|

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   });
   if (isError) return <div>Sorry There was an Error</div>;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between md:p-24 bg-white">
       <section className="w-full p-8 flex flex-col h-full justify-center">
         <Header />
         {isLoading ? <LoadingWheel /> : <VideosMain videos={data} />}
